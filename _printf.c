c3e45c703da3617f658f36afe38c6bdd7c71851a
lincolnMK <kingsmkumbwa@gmail.com>
2023-01-27 07:24:11 -0800

    functions.c added

--- _printf.c
+++ _printf.c
@@ -54,9 +54,12 @@ return (printed_chars);
 
 /**
 * print_buffer - Prints the contents of the buffer if it exist
+*
 * @buffer: Array of chars
+*
 * @buff_ind: Index at which to add next char, represents the length.
 */
+
 void print_buffer(char buffer[], int *buff_ind)
 {
 if (*buff_ind > 0)
